8476335e9b5f298bb40c513f30292aab4a74a321
Rigistik <165558235+Rigistik@users.noreply.github.com>
2024-12-08 23:58:09 +0300

    Update catalog_ui.py

--- catalog_ui.py
+++ catalog_ui.py
@@ -1,8 +1,5 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-class Product:
-    def __init__(self, name, price, description):
-        self.name = name
-        self.price = price
-        self.description = description
+
+               
