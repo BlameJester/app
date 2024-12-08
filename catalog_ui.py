59e38dae34ea106327d123e5886ac09c973615a7
Rigistik <165558235+Rigistik@users.noreply.github.com>
2024-12-09 00:01:56 +0300

    Add files via upload

--- catalog_ui.py
+++ catalog_ui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-
-               
+class Product:
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
+        xyw
